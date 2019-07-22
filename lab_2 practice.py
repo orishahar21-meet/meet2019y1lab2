22b4436a0046d981e5ce067aa9157f4394c3c5e5
orishahar21-meet <orishahar21@meet.mit.edu>
2019-07-22 03:18:03 -0400

    Stopping here

---
+++ lab_2 practice.py
@@ -0,0 +1,28 @@
+Python 3.6.8 (default, Jan 14 2019, 11:02:34) 
+[GCC 8.0.1 20180414 (experimental) [trunk revision 259383]] on linux
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+====== RESTART: /home/student/orishahar21_lab2/meet2019y1lab2/lab_2.py ======
+>>> type(259-33.0)
+<class 'float'>
+>>> type (4)
+<class 'int'>
+>>> type (four)
+Traceback (most recent call last):
+  File "<pyshell#2>", line 1, in <module>
+    type (four)
+NameError: name 'four' is not defined
+>>> type ('four')
+<class 'str'>
+>>> type(5/2.0)
+<class 'float'>
+>>> type(12 > 2*5)
+<class 'bool'>
+>>> type(color+3)
+Traceback (most recent call last):
+  File "<pyshell#7>", line 1, in <module>
+    type(color+3)
+NameError: name 'color' is not defined
+>>> type('color'*4)
+<class 'str'>
+>>> 
